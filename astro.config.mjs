dcb22e51eed40444992eaaab5fab7ebd1148b26b
David A <dluna217@gmail.com>
2026-02-22 22:50:40 -0500

    add SEO stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,18 @@ import vue from '@astrojs/vue';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jpabloastro.pages.dev/',
   image: {
     domains: ['cdn.sanity.io'],
   },
+
   adapter: cloudflare({
      imageService: 'compile'
   }),
+
+  integrations: [sitemap()],
 });

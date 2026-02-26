940e24b8566f991c9acace37304e04f673e7b5c8
David A <dluna217@gmail.com>
2026-02-26 12:33:12 -0500

    added info & change img & add wpp button floant

--- src/lib/dataCompany.ts
+++ src/lib/dataCompany.ts
@@ -33,12 +33,12 @@ export const myCompanyInfo: CompanyInfo = {
     legalName: 'Construcciones Juan Pablo II S.A.S.',
     description: ['Somos una empresa constructora joven en el Oriente Antioqueño, comprometida con la calidad, innovación y el desarrollo sostenible de nuestra región. Con 5 años de experiencia, transformamos sueños en realidades tangibles.','Fundada en el corazón del Oriente Antioqueño, Construcciones Juan Pablo II S.A.S. nació con la visión de elevar los estándares de construcción en la región. Durante este tiempo, hemos participado en numerosos proyectos urbanos y rurales, siempre priorizando la satisfacción de nuestros usuarios y el respeto por el entorno.'],
     address: 'San Carlos, Antioquia, Colombia',
-    phone: '+57 300 123 4567',
-    email: 'contacto@construccionesjuanpabloii.com',
+    phone: '+573116105615',
+    email: 'informacion@juanpabloll.com',
     socials: {
         instagram: { platform: 'instagram', url: 'https://instagram.com' },
         facebook: { platform: 'facebook', url: 'https://facebook.com' },
-        whatsapp: { platform: 'whatsapp', url: 'https://wa.me/573001234567?text='+encodeURIComponent('¡Hola! Me gustaría hacer una consulta sobre sus servicios.') },
+        whatsapp: { platform: 'whatsapp', url: 'https://wa.me/573116105615?text='+encodeURIComponent('¡Hola! Me gustaría hacer una consulta sobre sus servicios.') },
         youtube: { platform: 'youtube', url: 'https://youtube.com' }
     }
 };

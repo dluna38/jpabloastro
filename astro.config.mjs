97603e3e784538fabdec43e09f753b25c07e709c
David A <dluna217@gmail.com>
2026-02-26 15:39:59 -0500

    remove packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import vue from '@astrojs/vue';
-
 import cloudflare from '@astrojs/cloudflare';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
